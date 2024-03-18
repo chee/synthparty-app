59d339e8a34f3f80e2e9f9f6634f7baa8426e9bb
chee <yay@chee.party>
2024-03-18 20:02:59 +0000

    remove unused files

--- SynthParty/SynthPartyApp.swift
+++ SynthParty/SynthPartyApp.swift
@@ -13,9 +13,7 @@ struct SynthPartyApp: App {
 }
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        LocalDocumentsManager.copyIndexHtmlIfNeeded()
         return true
     }
 }
